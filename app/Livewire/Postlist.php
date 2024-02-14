ebe106ec39f1c799ec8a4032ccd26835112f57bd
GMwanda <george.mwanda@strathmore.edu>
2024-02-14 15:08:25 +0300

    searching for blogs

--- app/Livewire/Postlist.php
+++ app/Livewire/Postlist.php
@@ -4,6 +4,7 @@ namespace App\Livewire;
 
 use App\Models\Post;
 use Livewire\Attributes\Computed;
+use Livewire\Attributes\On;
 use Livewire\Attributes\Url;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -14,16 +15,25 @@ class Postlist extends Component
 
     #[Url()]
     public $sort = 'desc';
+    public $search = '';
 
     public function setSort($sort)
     {
         $this->sort = ($sort === 'desc') ? 'desc' : 'asc';
         $this->resetPage();
     }
+
+    #[On('search')]
+    public function updateSearch($search)
+    {
+        $this->search = $search;
+    }
+
+
     #[Computed()]
     public function posts()
     {
-        return Post::published()->orderBy('published_at', $this->sort)->paginate(10);
+        return Post::published()->orderBy('published_at', $this->sort)->where('title', 'like', "%{$this->search}%")->paginate(10);
     }
     public function render()
     {
---
+++ app/Livewire/SearchBox.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class SearchBox extends Component
+{
+
+    public $search = '';
+
+    public function updatedSearch()
+    {
+        $this->dispatch('search', search: $this->search);
+    }
+    public function render()
+    {
+        return view('livewire.search-box');
+    }
+}
